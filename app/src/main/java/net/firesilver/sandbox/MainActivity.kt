17a8b61c8e630902dd8631cbd12c0a5129901327
firecrow <fire@firecrow.com>
2023-02-17 13:02:43 +0000

    working to populate a list of cells

--- app/src/main/java/net/firesilver/sandbox/MainActivity.kt
+++ app/src/main/java/net/firesilver/sandbox/MainActivity.kt
@@ -25,6 +25,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import android.util.DisplayMetrics 
+import android.util.Log
 
 data class App (
     val name: String,
@@ -70,40 +71,56 @@ class MainActivity : AppCompatActivity() {
         val inflater: LayoutInflater =
             this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
+        Log.e("fcrow","filling grid")
+
         for(item in apps){
+            Log.e("fcrow","filling grid "+item.name)
 
             val cell = inflater.inflate(R.layout.cell, null)
             cell.layoutParams = LinearLayout.LayoutParams(cellHeight, cellWidth)
+            Log.e("fcrow","filling grid 2 "+item.name)
 
             var icon = cell.findViewById(R.id.icon_fg) as ImageView
+            Log.e("fcrow","filling grid 3 "+item.name)
             
             icon.setImageDrawable(item.icon)
+            Log.e("fcrow","filling grid 4 "+item.name)
 
             var matrix = ColorMatrix()
             matrix.setSaturation(0.0f)
             icon.setColorFilter(ColorMatrixColorFilter(matrix))
+            Log.e("fcrow","filling grid 5 "+item.name)
 
             cell.setOnClickListener(
                 View.OnClickListener { view -> onClick(item) }
             )
 
-            contentView.addView(cell)
+            Log.e("fcrow","filling grid 6 "+item.name)
+            try {
+                contentView.addView(cell)
+            }catch(e: Exception){
+                Log.e("fcrow","filling grid 7 "+e.toString())
+            }
+            Log.e("fcrow","filling grid 7 "+item.name)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("fcrow","hi there this is a test")
+
         val data = generateSystemList()
 
-        val grid = findViewById<ScrollView>(R.id.grid) as ScrollView 
+        val grid = findViewById<LinearLayout>(R.id.grid) as LinearLayout 
 
         val metrics = DisplayMetrics();
-        return getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
         val cellHeight = metrics.heightPixels / 10;
         val cellWidth = metrics.widthPixels / 6;
 
+        Log.e("fcrow","about to fill grid")
         fillGrid(grid, ArrayList<App>(data), cellHeight, cellWidth, { app ->
             this.getPackageManager().getLaunchIntentForPackage(app.info.packageName)
                 ?.let { this.startActivity(it) }
